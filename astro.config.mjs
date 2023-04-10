646556e4d7ae17d4ff12e168803383115e232da3
Timmy Willison <timmywil@users.noreply.github.com>
2023-04-10 12:24:06 -0400

    chore: remove superfluous code; add jQuery branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
+import node from '@astrojs/node'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://detect.jquery.com',
-  integrations: [mdx(), sitemap()],
-  output: 'server'
+  integrations: [sitemap()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  server: {
+    port: 8000
+  }
 })
