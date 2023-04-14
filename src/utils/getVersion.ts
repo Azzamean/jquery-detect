8c1707f2096d4bf592e7332660f85c42a9d87601
Timmy Willison <timmywil@users.noreply.github.com>
2023-04-14 17:43:11 -0400

    fix(getversion): accepts URLs without protocols

--- src/utils/getVersion.ts
+++ src/utils/getVersion.ts
@@ -7,13 +7,20 @@ declare global {
   }
 }
 
+function normalizeUrl(url: string) {
+  if (url.startsWith('http://') || url.startsWith('https://')) {
+    return url
+  }
+  return `http://${url}`
+}
+
 export default async function getVersion(url: string) {
   const launchOpts = process.env.CHROMIUM_FLAGS
     ? { args: process.env.CHROMIUM_FLAGS.split(/\s+/) }
     : {}
   const browser = await puppeteer.launch(launchOpts)
   const page = await browser.newPage()
-  await page.goto(url)
+  await page.goto(normalizeUrl(url))
 
   const version = await page.evaluate(() => {
     const $ = window.jQuery || window.$
